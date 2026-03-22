edb98853d4a698b123def204bebc559900665211
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-21 19:09:14 -0600

    Refactor latest episodes API handler
    Refactor API handler to use getLatest function and add CORS headers.

--- server/api/list/latest-episodes.get.ts
+++ server/api/list/latest-episodes.get.ts
@@ -1,12 +1,38 @@
-import { getLatestEpisodes } from "animeflv-scraper";
+import { getLatest } from "animeflv-scraper";
 
-export default defineCachedEventHandler(async () => {
+export default defineEventHandler(async (event) => {
 
-  const latest = await getLatestEpisodes();
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Max-Age": "86400"
+  });
 
-  return {
-    success: true,
-    data: latest
-  };
+  if (getMethod(event) === 'OPTIONS') {
+    event.node.res.statusCode = 204;
+    return 'ok';
+  }
 
-}, { maxAge: 300 });
+  try {
+    const latest = await getLatest();
+
+    if (!latest) {
+      throw createError({
+        statusCode: 404,
+        message: "No se encontraron episodios",
+      });
+    }
+
+    return {
+      success: true,
+      data: latest
+    };
+
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      message: "Error en el servidor de Anime",
+    });
+  }
+});
