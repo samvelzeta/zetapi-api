445a02fdebda979a90971581e59f34c5d2ac7092
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 12:13:03 -0600

    Refactor search API to improve CORS handling and response

--- server/api/search/by-filter.post.ts
+++ server/api/search/by-filter.post.ts
@@ -1,13 +1,41 @@
-export default defineEventHandler(async (event) => {
+import { searchAnimesByFilter, GenreEnum, StatusEnum, TypeEnum, OrderEnum } from "animeflv-scraper";
 
-  setResponseHeaders(event, {
-    "Access-Control-Allow-Origin": "*",
-  });
+const genres = Object.values(GenreEnum);
+const statuses = Object.values(StatusEnum);
+const types = Object.values(TypeEnum);
+const orders = Object.values(OrderEnum);
+
+export default defineEventHandler(async (event) => {
+  const apiKey = getHeader(event, "x-api-key");
+  if (apiKey !== process.env.API_KEY) {
+    throw createError({ statusCode: 401 });
+  }
+
+  setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Methods", "POST,OPTIONS");
+  setHeader(event, "Access-Control-Allow-Headers", "*");
+
+  if (event.method === "OPTIONS") return;
 
   const body = await readBody(event);
+  const { order, page } = getQuery(event) as { order: string, page: number };
+
+  const mappedOrder = {
+    default: "Por Defecto",
+    updated: "Recientemente Actualizados",
+    added: "Recientemente Agregados",
+    title: "Nombre A-Z",
+    rating: "Calificación"
+  }[order];
+
+  const search = await searchAnimesByFilter({ ...body, order: mappedOrder, page });
+
+  if (!search || !search?.media?.length) {
+    throw createError({ statusCode: 404 });
+  }
 
   return {
     success: true,
-    filters: body
+    data: search
   };
 });
