851207375bd21029170bc477946ec79dd609eaf2
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 14:36:53 -0600

    Update [slug].get.ts

--- server/api/anime/episode/[slug].get.ts
+++ server/api/anime/episode/[slug].get.ts
@@ -1,6 +1,19 @@
 import { getEpisode } from "animeflv-scraper";
 
 export default defineCachedEventHandler(async (event) => {
+  // 🌐 CORS
+  setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
+  setHeader(event, "Access-Control-Allow-Headers", "Content-Type, x-api-key");
+
+  // 🔥 PREFLIGHT
+  if (event.method === "OPTIONS") {
+    return {
+      status: 200
+    };
+  }
+
+  // 🔐 API KEY
   const apiKey = getHeader(event, "x-api-key");
 
   const envKey =
@@ -10,12 +23,6 @@ export default defineCachedEventHandler(async (event) => {
   if (!envKey || apiKey !== envKey) {
     throw createError({ statusCode: 401 });
   }
-
-  setHeader(event, "Access-Control-Allow-Origin", "*");
-  setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
-  setHeader(event, "Access-Control-Allow-Headers", "*");
-
-  if (event.method === "OPTIONS") return;
 
   const { slug } = getRouterParams(event) as { slug: string };
 
