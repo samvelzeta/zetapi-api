d42d956d422b4f792a2abfc421eb9a1c365e90c7
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-23 17:54:10 -0600

    Update filter.ts

--- server/utils/filter.ts
+++ server/utils/filter.ts
@@ -2,6 +2,8 @@ export async function filterWorkingServers(servers: any[]) {
   return servers.filter(s =>
     s?.embed &&
     !s.embed.includes(".css") &&
-    !s.embed.includes(".js")
+    !s.embed.includes(".js") &&
+    !s.embed.includes("logo") &&
+    !s.embed.includes("banner")
   );
 }
