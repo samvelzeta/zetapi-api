6f94cf1f14fc5813a35415d0b5b64b309ae3b988
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 20:28:12 -0600

    Create getServers.ts

---
+++ server/utils/getServers.ts
@@ -0,0 +1,41 @@
+import {
+  getAnimeFLVServers,
+  getJKAnimeServers,
+  getAnimeLHDServers,
+  getMonosChinosServers
+} from "./sources";
+
+export async function getAllServers({
+  slug,
+  number,
+  title,
+  lang
+}: {
+  slug: string;
+  number: number;
+  title: string;
+  lang: "sub" | "latino";
+}) {
+  let servers: any[] = [];
+
+  if (lang === "sub") {
+    const [flv, jk] = await Promise.all([
+      getAnimeFLVServers(slug, number),
+      getJKAnimeServers(slug, number)
+    ]);
+
+    servers = [...flv, ...jk];
+  }
+
+  if (lang === "latino") {
+    const [lhd, mono] = await Promise.all([
+      getAnimeLHDServers(title),
+      getMonosChinosServers(title)
+    ]);
+
+    servers = [...lhd, ...mono];
+  }
+
+  // quitar duplicados
+  return Array.from(new Map(servers.map(s => [s.embed, s])).values());
+}
