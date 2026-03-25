6fa15ccff437a9ee4871a3562ad98ca555e3e516
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-24 18:09:16 -0600

    Update filter.ts denuevo updet como siempre lo aocrtan

--- server/utils/filter.ts
+++ server/utils/filter.ts
@@ -1,26 +1,7 @@
 export async function filterWorkingServers(servers: any[]) {
-  const results = await Promise.all(
-    servers.map(async (s) => {
-      try {
-        const res = await fetch(s.embed, { method: "HEAD" });
-
-        const type = res.headers.get("content-type") || "";
-
-        if (
-          type.includes("video") ||
-          type.includes("mpegurl") ||
-          type.includes("mp4") ||
-          s.embed.includes("embed") // fallback
-        ) {
-          return s;
-        }
-
-        return null;
-      } catch {
-        return null;
-      }
-    })
+  return servers.filter(s =>
+    s?.embed &&
+    !s.embed.includes(".css") &&
+    !s.embed.includes(".js")
   );
-
-  return results.filter(Boolean);
 }
