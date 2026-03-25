7c0b21b2011b7cad0f8dac4db560b59f0a895622
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-24 22:50:03 -0600

    Update sources.ts que peasao tio

--- server/utils/sources.ts
+++ server/utils/sources.ts
@@ -3,7 +3,7 @@ import { $fetch } from "ofetch";
 
 // ======================
 // 🔥 NORMALIZAR SERVIDOR
-// ==========================
+// =======================
 function normalizeServer(name: string) {
   if (!name) return "server";
 
