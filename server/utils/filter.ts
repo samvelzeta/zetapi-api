0da2acae7865aefa1720c274e995de44956ce583
velzeta <160423772+samvelzeta@users.noreply.github.com>
2026-04-02 16:56:20 -0600

    Update filter.ts

--- server/utils/filter.ts
+++ server/utils/filter.ts
@@ -8,16 +8,16 @@ export async function filterWorkingServers(servers: any[]) {
 
     const url = s.embed.toLowerCase();
 
-    // ❌ basura
+    // ❌ basura real
     if (
+      url.includes("ads") ||
       url.includes("preview") ||
       url.includes("sample") ||
-      url.includes("ads") ||
       url.includes(".css") ||
       url.includes(".js")
     ) return false;
 
-    // ✔ HLS siempre válido
+    // ✔ HLS
     if (url.includes(".m3u8")) return true;
 
     // ✔ MP4
