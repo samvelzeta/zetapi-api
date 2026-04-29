bc0f4e4e654eb096da832afa92011a06a1e09c0e
velzeta <160423772+samvelzeta@users.noreply.github.com>
2026-04-28 20:46:49 -0600

    Update CHAT_ID for Seeke episode API

--- server/api/anime/episode-seeke.get.ts
+++ server/api/anime/episode-seeke.get.ts
@@ -1,7 +1,7 @@
 import { scrapeSeekeEpisode, generateCacheKey } from '../../utils/seekeScraper';
 
 const TELEGRAM_TOKEN = "8767809201:AAHhCa63uxL5yIMolv5CxMsgdgrPQOBuJgY";
-const CHAT_ID = "1749255245";
+const CHAT_ID = "-1003993096263";
 
 // ==============================
 // 🔥 LIMPIAR URL (IMPORTANTE)
